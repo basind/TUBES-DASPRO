49cca7c94f54504b1046370026dca8eb1a072548
basind <84658600+basind@users.noreply.github.com>
2022-04-07 18:38:05 +0700

    F6-9 dan database
    f6_9.py adalah file fungsi 6 sampai 9.
    Penambahan file .csv yang menjadi database.

---
+++ f6_9.py
@@ -0,0 +1,53 @@
+def ubahStok ():
+    # Fungsi mengubah stok game. Inputan ID GAME dan jumlah.
+    # Lalu memberi pesan yang berisi informasi pengurangan atau penjumlahan
+    # game, serta jumlah terkini game tersebut. 
+    # Hanya dapat dilakukan oleh admmin.
+
+    # KAMUS LOKAL
+
+    # ALGORITMA
+    ...
+
+
+def listGameToko ():
+    # Fungsi menampilkan list game di toko berdasarkan keyword pengguna. 
+    # Secara default diurutkan berdasar pada ID GAME dari terkecil ke terbesar. 
+    # Menerima inputan sebuah string "<keyword><+/->". 
+    # + menandakan diurutkan ascending, 
+    # - menandakan diurutkan descending. 
+    # dan akan menampilkan pesan error bila inputan tidak sesuai. 
+    # Dapat dilakukan admin dan user. 
+
+    # KAMUS LOKAL
+
+    # ALGORITMA
+    ...
+
+
+def beliGame ():
+    # Prosedur beliGame akan mengurangi stok dari game yang ID nya diinput oleh user, serta
+    # user memiliki saldo yang cukup dan belum memiliki game tersebut. 
+    # Bila belum dimiliki, namun saldo belum cukup maka akan diberikan pesan saldo tidak cukup. 
+    # Bila saldo cukup namun sudah dimiliki, akan ditampilkan pesan sudah dimiliki
+    # Bila saldo cuku dan belum dimiliki namun stok habis, maka akan ditampilkan pesan stok habis.
+    # Prosedur ini hanya dapat dilakukan oleh user.
+
+    # KAMUS LOKAL
+
+    # ALGORTIMA
+    ...
+
+
+def listGame ():
+    # Prosedur menampilkan game yang dimiliki user ascending berdasar game ID.
+    # Format : ID | Nama Game | Kategori | Tahun Rilis | Harga.
+    # Menampilkan pesan error jika user tidak memiliki game.
+    # Prosedur ini hanya dapat dilakukan oleh user. 
+
+    # KAMUS LOKAL
+
+    # ALGORITMA
+    ...
+
+
