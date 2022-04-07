47c4b679ed97e9dc0d2563ebdf5df53ae5bb90ec
sozy <thinpotatogithub@gmail.com>
2022-04-07 19:01:07 +0700

    Add f(10 - 13) skeleton

---
+++ f10_13.py
@@ -0,0 +1,46 @@
+def cariGameDimiliki():
+    # Spesifikasi
+    # ...
+
+    # KAMUS LOKAL
+    # ...
+
+    # ALGORITMA
+    # ...
+    pass
+
+
+def cariGameToko():
+    # Spesifikasi
+    # ...
+
+    # KAMUS LOKAL
+    # ...
+
+    # ALGORITMA
+    # ...
+    pass
+
+
+def topUp():
+    # Spesifikasi
+    # ...
+
+    # KAMUS LOKAL
+    # ...
+
+    # ALGORITMA
+    # ...
+    pass
+
+
+def lihatRiwayat():
+    # Spesifikasi
+    # ...
+
+    # KAMUS LOKAL
+    # ...
+
+    # ALGORITMA
+    # ...
+    pass
