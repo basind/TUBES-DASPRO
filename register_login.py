50658ae884037c45df552809a9c2d3e454335234
arieljovananda <86530612+arieljovananda@users.noreply.github.com>
2022-04-19 21:06:21 +0700

    Add files via upload

--- register_login.py
+++ register_login.py
@@ -1,1 +1,40 @@
+import helper as hp
+user = hp.potongDataCSV('user.csv')
+print (user)
 
+def registrasi():
+    nama = input('Masukkan nama: ')
+    un = input('Masukkan username: ')
+    pw = input('Masukkan password: ')
+    for i in un:
+        if (48 <= ord(i) <= 57) or (65 <= ord(i) <= 90) or (97 <= ord(i) <= 122) or (ord(i) == 45) or (ord(i) == 95):
+            pass
+        else:
+            print('Username tidak valid')
+    
+    for i in range (hp.panjang(user)):
+        if user[i][1] == un:
+             print(f'Username {un} sudah terpakai, silahkan menggunakan username lain.')
+        else:
+            return
+
+def login():
+    un = input('Masukkan username: ')
+    pw = input('Masukkan password: ')
+    check = False
+    for i in range (1,hp.panjang(user)):
+        if user[i][1] != un and user[i][3] != pw:
+            check = False
+        else:
+            check = True
+            x = i
+            break
+    if check == True:
+        print(f'Halo {user[x][2]}! Selamat datang di "Binomo"')
+    else:
+        print('Password atau username salah atau tidak ditemukan.')
+            
+
+
+
+             
---
+++ tambah_game.py
@@ -0,0 +1,14 @@
+def tambahgame(list):
+    ng = str(input('Masukkan nama game: '))
+    ka = str(input('Masukkan kategori: '))
+    tr = int(input('Masukkan tahun rilis: '))
+    ha = int(input('Masukkan harga: '))
+    sa = int(input('Masukkan stok awal: '))
+    if ng == '' or ka == '' or tr == '' or ha == '' or sa == '':
+        print('Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.')
+        tambahgame()
+    else:
+        print(f'Selamat! Berhasil menambahkan game {ng}.')
+    list+=[[ng,ka,tr,ha,sa]]
+
+tambahgame(list)
